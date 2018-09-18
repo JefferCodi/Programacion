05cbe8f1b900d1edf43d25f3a21ac74a457b529c
JefferLindao <jefferlindao@gmail.com>
2018-09-18 02:32:19 -0500

    js para el mantenimiento

--- localStorage/localStorage.js
+++ localStorage/localStorage.js
@@ -1,21 +1,68 @@
-var d = document
-
-function guardarIdioma() {
-	evento.preventDefault()
-
-	var datos = {
-		id : evento.target.id,
-		url : evento.target.href
-	}
+ var studentsArray = [];
+var selectedIndex = -1;
+function init() {
+    document.getElementById("tablerows").innerHTML = "";
+    if (localStorage.studentsRecord) {
+        studentsArray = JSON.parse(localStorage.studentsRecord);
+        for (var i = 0; i < studentsArray.length; i++) {
+            prepareTableCell(i, studentsArray[i].firstname, studentsArray[i].lastname, studentsArray[i].rollnum, studentsArray[i].subject);
+        }
+    }
+}
+function onRegisterPressed() {
+    var firstName = document.getElementById("firstname").value;
+    var lastName = document.getElementById("lastname").value;
+    var rollNum = document.getElementById("rollnum").value;
+    var subject = document.getElementById("subject").value;
+    var stuObj = {firstname: firstName, lastname: lastName, rollnum: rollNum, subject: subject};
+    if (selectedIndex === -1) {
+        studentsArray.push(stuObj);
+    } else {
+        studentsArray.splice(selectedIndex, 1, stuObj);
+    }
+    localStorage.studentsRecord = JSON.stringify(studentsArray);
+    init();
+    onClarPressed();
+}
+function prepareTableCell(index, firstName, lastName, rollNum, subject) {
+    var table = document.getElementById("tablerows");
+    var row = table.insertRow();
+    var firstNameCell = row.insertCell(0);
+    var lastNameCell = row.insertCell(1);
+    var rollNumCell = row.insertCell(2);
+    var subjectCell = row.insertCell(3);
+    var actionCell = row.insertCell(4);
+    firstNameCell.innerHTML = firstName;
+    lastNameCell.innerHTML = lastName;
+    rollNumCell.innerHTML = rollNum;
+    subjectCell.innerHTML = subject;
+    actionCell.innerHTML = '<button onclick="onEditPressed(' + index + ')">Edit</button><br/><button onclick="deleteTableRow(' + index + ')">Delete</button>';
+}
+function deleteTableRow(index) {
+    /*
+     var table = document.getElementById("regtable");
+     table.deleteRow(index+1);
+     */
+    studentsArray.splice(index, 1);
+    localStorage.studentsRecord = JSON.stringify(studentsArray);
+    init();
+}
+function onClarPressed() {
+    selectedIndex = -1;
+    document.getElementById("firstname").value = "";
+    document.getElementById("lastname").value = "";
+    document.getElementById("rollnum").value = "";
+    document.getElementById("subject").value = "Math";
+    document.getElementById("submit").innerHTML = "Register";
 }
 
-function recordarIdioma() {
-	var en = d.querySelector('#en'),
-	    es = d.querySelector('#es')
-
-	en.onclick = guardarIdioma
-	es.onclick = guardarIdioma
-
-	irAlIdioma()
+function onEditPressed(index) {
+    selectedIndex = index;
+    var stuObj = studentsArray[index];
+    document.getElementById("firstname").value = stuObj.firstname;
+    document.getElementById("lastname").value = stuObj.lastname;
+    document.getElementById("rollnum").value = stuObj.rollnum;
+    document.getElementById("subject").value = stuObj.subject;
+    document.getElementById("submit").innerHTML = "Update";
+    
 }
-window.onload = recordarIdioma
